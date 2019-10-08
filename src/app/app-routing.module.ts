d5e9353b246ace4e23b51167bd9ed380af0ab8e0
Angel Garcia Rodriguez <angelrodriguez@ucol.mx>
2019-10-08 07:37:40 -0500

    Layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AppContentComponent } from './layouts/app-content/app-content.component';
+import { LoginComponent } from './auth/login/login.component';
+import { CoursesListComponent } from './courses/courses-list/courses-list.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'admin',
+    component: AppContentComponent,
+    children: [
+      {
+        path: 'courses',
+        component: CoursesListComponent,
+        pathMatch: 'full',
+      }
+    ]
+  },
+
+  //no layout routes
+  { path: 'login', component: LoginComponent },
+  // otherwise redirect to home
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { AppHeaderComponent } from './layouts/app-header/app-header.component';
+import { AppBodyComponent } from './layouts/app-body/app-body.component';
+import { AppFooterComponent } from './layouts/app-footer/app-footer.component';
+import { CoursesListComponent } from './courses/courses-list/courses-list.component';
+import { AppContentComponent } from './layouts/app-content/app-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AppHeaderComponent,
+    AppBodyComponent,
+    AppFooterComponent,
+    CoursesListComponent,
+    AppContentComponent
   ],
   imports: [
     BrowserModule,
